535f9c582119be4b2ece91b40ea2b4019cdddb50
James Sample <james.sample@niva.no>
2018-02-20 15:05:24 +0100

    Resolve NDB conflicts; add alternative queries
    Deal with (some of the) duplication in the NDB tables. Also add
    functions get_station_props(), get_station_parameters2() and
    get_chemistry_values2(). The latter two query NIVADATABASE.WCV_CALK
    directly, hopefully avoiding the need to re-write lots of PL/SQL.

--- ndbview/ndb_queries.py
+++ ndbview/ndb_queries.py
@@ -61,7 +61,7 @@ def get_all_stations(engine):
            "  nivadatabase.stations b, "
            "  nivadatabase.station_types c, "
            "  niva_geometry.sample_points d "
-           "WHERE a.station_id = b.station_id "
+           "WHERE a.station_id    = b.station_id "
            "AND b.station_type_id = c.station_type_id "
            "AND b.geom_ref_id     = d.sample_point_id "
            "ORDER BY a.station_id")
@@ -74,6 +74,48 @@ def get_all_stations(engine):
 
     return df
 
+def get_station_props(stn_ids, engine):
+    """ Get properties for specified stations from the NDB.
+    
+    Args:
+        stn_ids: Array-like. 1D integer array of station IDs
+        engine:  Obj. Active NDB "engine" object
+        
+    Returns:
+        Dataframe
+    """ 
+    # Build query
+    bind_pars = ','.join(':%d' % i for i in xrange(len(stn_ids)))
+    
+    # Query db
+    sql = ("SELECT DISTINCT a.station_id, "
+           "  a.station_code, "
+           "  a.station_name, "
+           "  c.station_type, "
+           "  d.latitude, "
+           "  d.longitude "
+           "FROM nivadatabase.projects_stations a, "
+           "  nivadatabase.stations b, "
+           "  nivadatabase.station_types c, "
+           "  niva_geometry.sample_points d "
+           "WHERE a.station_id    = b.station_id "
+           "AND b.station_type_id = c.station_type_id "
+           "AND b.geom_ref_id     = d.sample_point_id "
+           "AND a.station_id     IN (%s)" % bind_pars)
+    df = pd.read_sql(sql, params=stn_ids, con=engine)
+    
+    # Station IDs are unique locations, but they can have different names
+    # in different projects. This code drops duplicated IDs, but chooses
+    # station name and code at random from among the duplicates
+    df.drop_duplicates(subset='station_id', inplace=True)
+
+    # Decode special characters
+    for col in df.columns:
+        if df[col].dtype == object:
+            df[col] = df[col].str.decode('Latin-1')
+
+    return df   
+    
 def get_project_stations(proj_df, engine):
     """ Get stations asscoiated with selected projects.
     
@@ -90,10 +132,9 @@ def get_project_stations(proj_df, engine):
     proj_df['project_id'].drop_duplicates(inplace=True)
     proj_ids = proj_df['project_id'].values.astype(int)
 
-    # Build query
-    bind_pars = ','.join(':%d' % i for i in xrange(len(proj_ids)))
-    
     # Query db
+    bind_pars = ','.join(':%d' % i for i in xrange(len(proj_ids)))    
+
     sql = ("SELECT DISTINCT a.station_id, "
            "  a.station_code, "
            "  a.station_name, "
@@ -115,6 +156,11 @@ def get_project_stations(proj_df, engine):
            "ORDER BY a.station_id" % bind_pars)
     df = pd.read_sql(sql, params=proj_ids, con=engine)
 
+    # Station IDs are unique locations, but they can have different names
+    # in different projects. This code drops duplicated IDs, but chooses
+    # station name and code at random from among the duplicates
+    df.drop_duplicates(subset='station_id', inplace=True)
+    
     # Decode special characters
     for col in df.columns:
         if df[col].dtype == object:
@@ -122,7 +168,7 @@ def get_project_stations(proj_df, engine):
                    
     return df
 
-def get_station_parameters(stn_df, engine):
+def get_station_parameters(stn_df, st_dt, end_dt, engine):
     """ Gets the list of available water chemistry parameters for the
         selected stations.
 
@@ -139,33 +185,101 @@ def get_station_parameters(stn_df, engine):
     stn_df['station_id'].drop_duplicates(inplace=True)
     stn_ids = stn_df['station_id'].values.astype(int)
 
-    # Build query
-    bind_pars = ','.join(':%d' % i for i in xrange(len(stn_ids)))
-
+    # Convert dates
+    st_dt = dt.datetime.strptime(st_dt, '%Y-%m-%d')
+    end_dt = dt.datetime.strptime(end_dt, '%Y-%m-%d')
+    
     # Query db
+    bind_pars = ','.join(':%d' % i for i in xrange(len(stn_ids)))
+    
     sql = ("SELECT parameter_id, "
            "  name AS parameter_name, "
            "  unit "
            "FROM NIVADATABASE.WC_PARAMETER_DEFINITIONS "
            "WHERE PARAMETER_ID IN "
            "  ( SELECT DISTINCT parameter_id "
-           "  FROM NIVADATABASE.WC_PARAMETERS_METHODS "
-           "  WHERE METHOD_ID IN "
+           "    FROM NIVADATABASE.WC_PARAMETERS_METHODS "
+           "    WHERE METHOD_ID IN "
            "    ( SELECT DISTINCT b.method_id "
-           "    FROM nivadatabase.water_samples a, "
+           "      FROM nivadatabase.water_samples a, "
            "      NIVADATABASE.WATER_CHEMISTRY_VALUES b "
-           "    WHERE a.station_id   IN (%s) "
-           "    AND a.WATER_SAMPLE_ID = b.WATER_SAMPLE_ID "
+           "      WHERE a.station_id   IN (%s) "
+           "      AND a.sample_date    >= :st_dt "
+           "      AND a.sample_date    <= :end_dt " 
+           "      AND b.approved        = 1"
+           "      AND a.WATER_SAMPLE_ID = b.WATER_SAMPLE_ID "
            "    ) "
            "  ) "
            "ORDER BY name" % bind_pars)
-    df = pd.read_sql(sql, params=stn_ids, con=engine)
+
+    par_dict = {'end_dt':end_dt,
+                'st_dt':st_dt}
+    bind_dict = {'%d' % idx:item for idx, item in enumerate(stn_ids)}
+    par_dict.update(bind_dict)    
+    df = pd.read_sql(sql, params=par_dict, con=engine) 
 
     # Decode special characters
     for col in df.columns:
         if df[col].dtype == object:
             df[col] = df[col].str.decode('Latin-1')
                    
+    return df
+
+def get_station_parameters2(stn_df, st_dt, end_dt, engine):
+    """ Gets the list of available water chemistry parameters for the
+        selected stations.
+
+        NOTE: This is an alternative to get_station_parameters().
+        
+        It looks as though Tore/Roar have already written code to 
+        summarise data into NIVADATABASE.WCV_CALK. Assuming this table 
+        is reliable, it is easier to query directly than to refactor lots
+        of PL/SQL into Python.
+
+    Args:
+        stn_df: Dataframe. Must have a column named 'station_id' with
+                the station IDs of interest
+        st_dt: Str. Format 'YYYY-MM-DD'
+        end_dt:   Str. Format 'YYYY-MM-DD'
+        engine: Obj. Active NDB "engine" object
+        
+    Returns:
+        Dataframe
+    """ 
+    # Get stn IDs
+    assert len(stn_df) > 0, 'ERROR: Please select at least one station.'
+    stn_df['station_id'].drop_duplicates(inplace=True)
+    stn_ids = stn_df['station_id'].values.astype(int)
+
+    # Convert dates
+    st_dt = dt.datetime.strptime(st_dt, '%Y-%m-%d')
+    end_dt = dt.datetime.strptime(end_dt, '%Y-%m-%d')
+    
+    # Query db
+    bind_pars = ','.join(':%d' % i for i in xrange(len(stn_ids)))    
+
+    bind_pars = ','.join(':%d' % i for i in xrange(len(stn_ids)))
+    sql = ("SELECT DISTINCT parameter_id, "
+           "  name AS parameter_name, "
+           "  unit "
+           "FROM nivadatabase.wcv_calk "
+           "WHERE station_id IN (%s) "
+           "AND sample_date  >= :st_dt "
+           "AND sample_date  <= :end_dt " 
+           "ORDER BY name, "
+           "  unit" % bind_pars)
+
+    par_dict = {'end_dt':end_dt,
+                'st_dt':st_dt}
+    bind_dict = {'%d' % idx:item for idx, item in enumerate(stn_ids)}
+    par_dict.update(bind_dict)    
+    df = pd.read_sql(sql, params=par_dict, con=engine)    
+    
+    # Decode special characters
+    for col in df.columns:
+        if df[col].dtype == object:
+            df[col] = df[col].str.decode('Latin-1')
+            
     return df
 
 def get_chemistry_values(stn_df, par_df, st_dt,
@@ -190,6 +304,9 @@ def get_chemistry_values(stn_df, par_df, st_dt,
     assert len(stn_df) > 0, 'ERROR: Please select at least one station.'
     stn_df['station_id'].drop_duplicates(inplace=True)
     stn_ids = stn_df['station_id'].values.astype(int)
+    
+    # Get stn properties
+    stn_props = get_station_props(stn_ids, engine)
 
     # Get par IDs
     assert len(par_df) > 0, 'ERROR: Please select at least one parameter.'
@@ -215,7 +332,8 @@ def get_chemistry_values(stn_df, par_df, st_dt,
            "  b.name AS parameter_name, "
            "  b.unit, "
            "  c.flag1, "
-           "  (c.value*d.conversion_factor) AS value "
+           "  (c.value*d.conversion_factor) AS value, "
+           "  c.entered_date "
            "FROM nivadatabase.water_samples a, "
            "  NIVADATABASE.WC_PARAMETER_DEFINITIONS b, "
            "  NIVADATABASE.WATER_CHEMISTRY_VALUES c, "
@@ -229,7 +347,6 @@ def get_chemistry_values(stn_df, par_df, st_dt,
            "AND a.sample_date      >= :st_dt "
            "AND a.sample_date      <= :end_dt" % (bind_stns, bind_pars))
 
-    # Build query params
     par_dict = {'end_dt':end_dt,
                 'st_dt':st_dt}
     bind_stn_dict = {'%d' % idx:item for idx, item in enumerate(stn_ids)}
@@ -245,8 +362,15 @@ def get_chemistry_values(stn_df, par_df, st_dt,
             df[col] = df[col].str.decode('Latin-1')
 
     # Drop exact duplicates (i.e. including value)
-    df.drop_duplicates(inplace=True)
-
+    df.drop_duplicates(subset=['station_id',
+                               'sample_date',
+                               'depth1',
+                               'depth2',
+                               'parameter_name',
+                               'unit',
+                               'flag1',
+                               'value'], inplace=True)
+    
     # Check for conflicting duplicates (i.e. same location, different value)
     dup_df = df[df.duplicated(subset=['station_id',
                                       'sample_date',
@@ -259,7 +383,8 @@ def get_chemistry_values(stn_df, par_df, st_dt,
                                                            'depth1',
                                                            'depth2',
                                                            'parameter_name',
-                                                           'unit']) 
+                                                           'unit',
+                                                           'entered_date']) 
 
     if len(dup_df) > 0:
         print ('    WARNING\n    The database contains duplicated values for some station-'
@@ -267,6 +392,9 @@ def get_chemistry_values(stn_df, par_df, st_dt,
                'will be used, but you should check the repeated values are not '
                'errors.\n    The duplicated entries are returned in a separate '
                'dataframe.\n')
+        
+        # Choose most recent record for each duplicate
+        df.sort_values(by='entered_date', inplace=True, ascending=True)
         
         # Drop duplicates
         df.drop_duplicates(subset=['station_id',
@@ -278,19 +406,23 @@ def get_chemistry_values(stn_df, par_df, st_dt,
                            keep='last', inplace=True)
 
     # Restructure data
-    df['par_unit'] = df['parameter_name'] + '_' + df['unit']
+    del df['entered_date']
+    df['parameter_name'].fillna('', inplace=True)
+    df['unit'].fillna('', inplace=True)
+    df['par_unit'] = (df['parameter_name'].astype(unicode) + '_' +
+                      df['unit'].astype(unicode))
     del df['parameter_name'], df['unit']
 
     # Include LOD flags?
     if lod_flags:
-        df['flag1'] = df['flag1'].fillna('')
-        df['value'] = df['flag1'].astype(str) + df['value'].astype(str)
+        df['flag1'].fillna('', inplace=True)
+        df['value'] = df['flag1'].astype(unicode) + df['value'].astype(unicode)
         del df['flag1']
         
     else: # Ignore flags
         del df['flag1']
 
-    # Unstack
+    # Unstack   
     df.set_index(['station_id', 'sample_date', 'depth1',
                   'depth2', 'par_unit'], inplace=True)
     df = df.unstack(level='par_unit')
@@ -300,5 +432,189 @@ def get_chemistry_values(stn_df, par_df, st_dt,
     df.index.name = ''
     df.columns = (list(df.columns.get_level_values(0)[:4]) + 
                   list(df.columns.get_level_values(1)[4:]))
+    df.sort_values(by=['station_id', 'sample_date'],
+                   inplace=True)
+    
+    # Join stn properties
+    # Props to include
+    props = ['station_id', 'station_code', 'station_name']
+    df = pd.merge(df, stn_props[props], how='left', on='station_id')
+    
+    # Reorder
+    cols = [col for col in df.columns if col not in props]
+    cols = props + cols
+    df = df[cols]    
+    
+    return (df, dup_df)
+
+def get_chemistry_values2(stn_df, par_df, st_dt,
+                          end_dt, lod_flags, engine):
+    """ Get water chemistry data for selected station-parameter-
+        date combinations. 
+        
+        NOTE: This is an alternative to get_chemistry_values().
+        
+        It looks as though Tore/Roar have already written code to deal
+        with duplicates etc., similar to what I've implemented above in
+        get_chemistry_values(). See NIVADATABASE.PKG_WC_COMPUTED (which 
+        updates NIVADATABASE.WCV_CALK) for details.
+        
+        I think there may still be some issues with WCV_CALK, but one
+        advantage is that it includes *some* of the "calculated" 
+        parameters available in RESA2 (e.g. ANC). 
+        
+        Assuming this table is reliable, it is easier to query directly
+        than to refactor lots of PL/SQL into Python.
+        
+    Args:
+        stn_df:    Dataframe. Must have a column named 'station_id' with
+                   the station IDs of interest 
+        par_df:    Dataframe. Must have a column named 'parameter_id' with
+                   the parameter IDs of interest
+        st_dt:     Str. Format 'YYYY-MM-DD'
+        end_dt:    Str. Format 'YYYY-MM-DD'
+        lod_flags: Bool. Whether to include LOD flags in output
+        engine:    Obj. Active NDB "engine" object
+        
+    Returns:
+        Tuple of dataframes (wc_df, dup_df)   
+    """
+    # Get stn IDs
+    assert len(stn_df) > 0, 'ERROR: Please select at least one station.'
+    stn_df['station_id'].drop_duplicates(inplace=True)
+    stn_ids = stn_df['station_id'].values.astype(int)
+    
+    # Get stn properties
+    stn_props = get_station_props(stn_ids, engine)
+
+    # Get par IDs
+    assert len(par_df) > 0, 'ERROR: Please select at least one parameter.'
+    par_df['parameter_id'].drop_duplicates(inplace=True)
+    par_ids = par_df['parameter_id'].values.astype(int)
+
+    # Convert dates
+    st_dt = dt.datetime.strptime(st_dt, '%Y-%m-%d')
+    end_dt = dt.datetime.strptime(end_dt, '%Y-%m-%d')
+       
+    # Number from 0 to n_stns
+    bind_stns = ','.join(':%d' % i for i in xrange(len(stn_ids)))
+    
+    # Number from n_stns to (n_stns+n_params)
+    bind_pars = ','.join(':%d' % i for i in xrange(len(stn_ids), 
+                                                   len(stn_ids) + len(par_ids)))
+
+    # Query db
+    sql = ("SELECT a.station_id, "
+           "  a.station_code, "
+           "  a.station_name, "
+           "  b.sample_date, "
+           "  b.depth1, " 
+           "  b.depth2, "
+           "  b.name AS parameter_name, "
+           "  b.unit, "
+           "  b.flag1, "
+           "  b.value, "
+           "  b.entered_date "
+           "FROM nivadatabase.projects_stations a, "
+           "  nivadatabase.wcv_calk b "
+           "WHERE a.station_id  = b.station_id "
+           "AND a.station_id   IN (%s) "
+           "AND b.parameter_id IN (%s) "
+           "AND sample_date    >= :st_dt "
+           "AND sample_date    <= :end_dt" % (bind_stns, bind_pars))
+
+    par_dict = {'end_dt':end_dt,
+                'st_dt':st_dt}
+    bind_stn_dict = {'%d' % idx:item for idx, item in enumerate(stn_ids)}
+    bind_par_dict = {'%d' % (idx + len(stn_ids)):item 
+                     for idx, item in enumerate(par_ids)}
+    par_dict.update(bind_stn_dict)
+    par_dict.update(bind_par_dict)    
+    df = pd.read_sql(sql, params=par_dict, con=engine)
+
+    # Decode special characters
+    for col in df.columns:
+        if df[col].dtype == object:
+            df[col] = df[col].str.decode('Latin-1')
+
+    # Drop exact duplicates (i.e. including value)
+    df.drop_duplicates(subset=['station_id',
+                               'sample_date',
+                               'depth1',
+                               'depth2',
+                               'parameter_name',
+                               'unit',
+                               'flag1',
+                               'value'], inplace=True)
+
+    # Check for duplicates
+    dup_df = df[df.duplicated(subset=['station_id',
+                                      'sample_date',
+                                      'depth1',
+                                      'depth2',
+                                      'parameter_name',
+                                      'unit'], 
+                              keep=False)].sort_values(by=['station_id',
+                                                           'sample_date',
+                                                           'depth1',
+                                                           'depth2',
+                                                           'parameter_name',
+                                                           'unit',
+                                                           'entered_date']) 
+
+    if len(dup_df) > 0:
+        print ('    WARNING\n    The database contains duplicated values for some station-'
+               'date-parameter combinations.\n    Only the most recent values '
+               'will be used, but you should check the repeated values are not '
+               'errors.\n    The duplicated entries are returned in a separate '
+               'dataframe.\n')
+
+        # Choose most recent record for each duplicate
+        df.sort_values(by='entered_date', inplace=True, ascending=True)
+        
+        # Drop duplicates
+        df.drop_duplicates(subset=['station_id',
+                                   'sample_date',
+                                   'depth1',
+                                   'depth2',
+                                   'parameter_name',
+                                   'unit'],
+                           keep='last', inplace=True)
+        
+    # Restructure data
+    del df['entered_date']
+    df['parameter_name'].fillna('', inplace=True)
+    df['unit'].fillna('', inplace=True)
+    df['par_unit'] = (df['parameter_name'].astype(unicode) + '_' +
+                      df['unit'].astype(unicode))
+    del df['parameter_name'], df['unit']
+
+    # Include LOD flags?
+    if lod_flags:
+        df['flag1'].fillna('', inplace=True)
+        df['value'] = df['flag1'].astype(unicode) + df['value'].astype(unicode)
+        del df['flag1']
+        
+    else: # Ignore flags
+        del df['flag1']
+
+    # Unstack   
+    df.set_index(['station_id', 
+                  'station_code',
+                  'station_name',
+                  'sample_date',
+                  'depth1',
+                  'depth2', 
+                  'par_unit'], 
+                 inplace=True)
+    df = df.unstack(level='par_unit')
+
+    # Tidy
+    df.reset_index(inplace=True)
+    df.index.name = ''
+    df.columns = (list(df.columns.get_level_values(0)[:6]) + 
+                  list(df.columns.get_level_values(1)[6:]))
+    df.sort_values(by=['station_id', 'sample_date'],
+                   inplace=True)   
     
     return (df, dup_df)
