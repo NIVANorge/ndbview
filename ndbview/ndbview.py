f1e3e9805736ee1bd4bd7cf3142798f6c54e5389
James Sample <james.sample@niva.no>
2018-03-02 15:32:05 +0100

    Tidy end points

--- ndbview/ndbview.py
+++ ndbview/ndbview.py
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------------
 # Name:        ndbview.py
-# Purpose:     Flask app providing end points for simple NIVADATABASE UI.
+# Purpose:     Flask app providing end points for a simple NIVADATABASE UI.
 #
 # Author:      James Sample
 #
@@ -12,9 +12,7 @@
 
         http://flask.pocoo.org/docs/0.12/tutorial/
 
-    The main aim is to provde "end points" for a new NIVADATABASE frontend, but
-    I've also used it as an opportunity to play with Flask for the first time.
-    Some tidying is probably required!
+    The main aim is to provide "end points" for a new NIVADATABASE frontend.
 """
 import os
 import pandas as pd
@@ -31,6 +29,7 @@ from flask import url_for, abort, render_template, flash
 app = Flask(__name__) 
 
 # Set config
+# The password below has read-only access to a limited number of db tables
 app.config.update(dict(
     DATABASE=r'oracle+cx_oracle://%s:%s@nivabase:1521/nivabase',
     SECRET_KEY='development key',
@@ -76,12 +75,11 @@ def get_engine():
 @app.route('/get_all_stations')
 def get_all_stations():
     """ Gets ALL stations from the NIVADATABASE.
+    
+        WARNING: There are around 30k stations, so this is slow.
 
     Returns:
-        JSON.
-        
-        For testing, can uncomment the alternative 'return' statement
-        in the code below to render an HTML table instead of raw JSON.       
+        JSON.    
     """
     # Get db engine for this session
     engine = get_engine()
@@ -90,14 +88,12 @@ def get_all_stations():
     stn_df = ndb_queries.get_all_stations(engine)
     stn_df = stn_df[['station_id', 'station_code', 'station_name',
                      'longitude', 'latitude']]
-    stn_df.columns = ['id', 'c', 'n', 'x', 'y']
-
-    # Reformat
-    data = stn_df.to_dict(orient='records')
     
-#    return render_template('show_projects_stations.html',
-#                           tables=[stn_df.to_html(), ],
-#                           titles=['Stations', ])
+    # Convert np.nan to None for valid JSON
+    stn_df = stn_df.where((pd.notnull(stn_df)), None)
+    
+    # Reformat
+    data = stn_df.to_dict(orient='list')
 
     return jsonify(data)
 
@@ -106,34 +102,32 @@ def get_all_projects():
     """ Gets ALL projects from the NIVADATABASE.
 
     Returns:
-        JSON.
-        
-        For testing, can uncomment the alternative 'return' statement
-        in the code below to render an HTML table instead of raw JSON.        
+        JSON.       
     """
     # Get db engine for this session
     engine = get_engine()
 
     # Get projects
     proj_df = ndb_queries.get_all_projects(engine)
-    proj_df = proj_df[['project_id', 'o_number', 'project_name']]
-    proj_df.columns = ['id', 'o', 'n']
+    proj_df = proj_df[['project_id', 'project_name']]
 
-    # Reformat
-    data = proj_df.to_dict(orient='records')
+    # Convert np.nan to None for valid JSON
+    proj_df = proj_df.where((pd.notnull(proj_df)), None)
     
-#    return render_template('show_projects_stations.html',
-#                           tables=[proj_df.to_html(), ],
-#                           titles=['Projects', ])
+    # Reformat
+    data = proj_df.to_dict(orient='list')
 
     return jsonify(data)
 
 @app.route('/get_project_stations', methods=['POST',])
 def get_project_stations():
     """ Gets stations for the selected projects. Assumes data is POSTed
-        as a JSON array of integers names 'id':
+        as a JSON array of integers named 'project_id'. Optionally, can
+        also pass a 'drop_dups' parameter (set to 'false' by default). 
+        See docstring in ndb_queries.py for details:
 
-            {"id":[87, 88, 89]}
+            {"project_id":[87, 88, 89],
+             "drop_dups": false}
 
     Returns:
         Stations table in JSON format
@@ -145,27 +139,70 @@ def get_project_stations():
     
     # Parse posted data
     sel_proj_json = request.get_json()
-    sel_proj_df = pd.DataFrame(sel_proj_json)
+    sel_proj_df = pd.DataFrame({'project_id':sel_proj_json['project_id']})
+    try:
+        drop_dups = sel_proj_json['drop_dups']
+    except KeyError:
+        drop_dups = False
 
     # Get stations
-    stn_df = ndb_queries.get_project_stations(sel_proj_df, engine)
+    stn_df = ndb_queries.get_project_stations(sel_proj_df, engine, 
+                                              drop_dups=drop_dups)
     stn_df = stn_df[['station_id', 'station_code', 'station_name',
                      'longitude', 'latitude']]
-    stn_df.columns = ['id', 'c', 'n', 'x', 'y']
 
+    # Convert np.nan to None for valid JSON
+    stn_df = stn_df.where((pd.notnull(stn_df)), None)
+    
     # Reformat
-    data = stn_df.to_dict(orient='records')
+    data = stn_df.to_dict(orient='list')
+
+    return jsonify(data)
+
+@app.route('/get_station_projects', methods=['POST',])
+def get_station_projects():
+    """ Updates the projects list based on the projects originally selected
+        and the current station list. Assumes data is POSTed as JSON arrays
+        of integers for the currently selected 'project_id's and 
+        'station_id's. 
+
+            {"project_id":[87, 88, 89],
+             "station_id":[9456, 9457, 9458]}
+
+    Returns:
+        Set intersection of 'project_id' array and projects associated with
+        'station_id' array
+
+    NOTE: Can test using 'Postman'
+    """
+    # Get db engine for this session
+    engine = get_engine()
+    
+    # Parse posted data
+    sel_json = request.get_json()
+    sel_proj_df = pd.DataFrame({'project_id':sel_json['project_id']})
+    sel_stn_df = pd.DataFrame({'station_id':sel_json['station_id']})
+
+    # Get stations
+    proj_df = ndb_queries.get_station_projects(sel_stn_df, sel_proj_df, engine)
+    proj_df = proj_df[['project_id', 'project_name']]
+
+    # Convert np.nan to None for valid JSON
+    proj_df = proj_df.where((pd.notnull(proj_df)), None)
+    
+    # Reformat
+    data = proj_df.to_dict(orient='list')
 
     return jsonify(data)
 
 @app.route('/get_station_parameters', methods=['POST',])
 def get_station_parameters():
     """ Gets water chemistry parameters for the selected stations. Assumes
-        data is POSTed as a JSON in the following format:
+        data is POSTed as JSON in the following format:
 
-            {"st_dt":   "1990-01-01",
-             "end_dt":  "2010-12-31",
-             "id":[3561, 3562, 3563]}
+            {"st_dt":     "1990-01-01",
+             "end_dt":    "2010-12-31",
+             "station_id":[3561, 3562, 3563]}
 
     Returns:
         Parameters table in JSON format
@@ -179,15 +216,17 @@ def get_station_parameters():
     sel_stn_json = request.get_json()
     st_dt = sel_stn_json['st_dt']
     end_dt = sel_stn_json['end_dt']
-    sel_stn_df = pd.DataFrame({'id':sel_stn_json['id']})
+    sel_stn_df = pd.DataFrame({'station_id':sel_stn_json['station_id']})
 
     # Get parameters
     par_df = ndb_queries.get_station_parameters2(sel_stn_df, st_dt, end_dt, engine)
     par_df = par_df[['parameter_id', 'parameter_name', 'unit']]
-    par_df.columns = ['id', 'n', 'u']
+    
+    # Convert np.nan to None for valid JSON
+    par_df = par_df.where((pd.notnull(par_df)), None)
 
     # Reformat
-    data = par_df.to_dict(orient='records')
+    data = par_df.to_dict(orient='list')
 
     return jsonify(data)
 
@@ -197,12 +236,16 @@ def get_chemistry_values():
         date combinations. Assumes data is POSTed as nested JSON in the
         following format:
 
-            {"st_dt": "1990-01-01",
-             "end_dt":"2010-12-31",
-             "lods":  true,
-             "stns":  [3561, 3562, 3563],
-             "pars":  [7, 8, 12, 244]}  
-             
+            {"st_dt":       "1990-01-01",
+             "end_dt":      "2010-12-31",
+             "lods":        true,
+             "drop_dups":   false,
+             "station_id":  [3561, 3562, 3563],
+             "parameter_id":[7, 8, 12, 244]}  
+
+        If 'lods' is omitted, it is assumed to be 'true'; if 'drop_dups'
+        is omitted, it is assumed to be 'false'.
+        
     Returns:
         Water chemistry table in JSON format
 
@@ -213,18 +256,29 @@ def get_chemistry_values():
     
     # Parse posted data
     sel_json = request.get_json()
-    stn_df = pd.DataFrame({'stns':sel_json['stns']})
-    par_df = pd.DataFrame({'pars':sel_json['pars']})
+    stn_df = pd.DataFrame({'station_id':sel_json['station_id']})
+    par_df = pd.DataFrame({'parameter_id':sel_json['parameter_id']})
     st_dt = sel_json['st_dt']
     end_dt = sel_json['end_dt']
-    lod_flags = sel_json['lods']
+    try:
+        drop_dups = sel_json['drop_dups']
+    except KeyError:
+        drop_dups = False
+    try:
+        lod_flags = sel_json['lods']
+    except KeyError:
+        lod_flags = True
 
     # Get chemistry values
     wc_df, dup_df = ndb_queries.get_chemistry_values2(stn_df, par_df,
                                                       st_dt, end_dt,
-                                                      lod_flags, engine)
+                                                      lod_flags, engine,
+                                                      drop_dups=drop_dups)
+    
+    # Convert np.nan to None for valid JSON
+    wc_df = wc_df.where((pd.notnull(wc_df)), None)
 
     # Reformat
-    data = wc_df.to_dict(orient='records')
+    data = wc_df.to_dict(orient='list')
 
     return jsonify(data)
